1ddb4d8a8da4770cd5310f3604ee241ed3ffec3f
thongnguyen <thongnguyen@pixelcent.com>
2023-08-20 15:58:13 +0700

    Upload buoi 1

--- Buoi1/main.py
+++ Buoi1/main.py
@@ -1,9 +1,37 @@
-print("Hello World!")
+# print("Hello World!")
 
-# Day la comment trong Python
+# # Day la comment trong Python
 
-name = "Thong"
-age = 14
-address = "1/3 Nguyen Van Bao, Go Vap, HCM"
+# name = "Thong"
+# age = 14
+# address = "1/3 "
 
-print(name)
+# print(name)
+
+# -----------------------------
+# English
+# snake_case
+
+# fullname = input("Moi nhap vao ten cua ban: ")
+
+# print("Hello " + fullname)
+
+# weak type
+
+# name = "Thong"
+
+# name = 123 + 1
+
+# print(name)
+
+from turtle import *
+
+shape("turtle")
+
+forward(100) # buoc
+
+right(90) # do
+
+forward(100) # buoc
+
+mainloop()
