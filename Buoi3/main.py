253e56ecd3a17f7a0fc3eccc7f6fdfd598fdfa2f
thongnguyen <thongnguyen@pixelcent.com>
2023-09-20 17:03:15 +0700

    Add tai lieu

---
+++ Buoi3/main.py
@@ -0,0 +1,16 @@
+result = 0
+i = 1
+
+while i <= 3:
+    result = result + i
+    i = i + 1
+
+# lan chay thu: 1
+# result = 1
+# i = 2
+
+# lan chay thu: 2
+# result = 3
+# i = 3
+
+print("Result: ", result)
---
+++ Buoi4/main.py
@@ -0,0 +1,15 @@
+
+# 1 dấu = là phép gán
+# 2 dấu = (==) là phép so sánh
+
+# number / string / boolean
+
+a = 10
+b = 10
+
+if ((a > b) or (a == b)):
+    print("A >= B")
+elif (a < b):
+    print("A < B")
+else:
+    print("A = B")
