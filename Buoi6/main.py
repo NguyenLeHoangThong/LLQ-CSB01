6c47270025d8c8876e648e034b0bffba85d18eae
thongnguyen <thongnguyen@pixelcent.com>
2023-10-01 14:52:50 +0700

    Upload bai 6

---
+++ Buoi6/main.py
@@ -0,0 +1,8 @@
+students = ["Thong", "Hien", 20, "Van", 15, 60, "Nam"]
+#students = list(("Thong", "Hien", 20, "Van", 15))
+
+i = 0
+
+while (i < len(students)):
+    print(students[i])
+    i = i + 1
