651380f1c4d4fc976d88d9b2ca3ea8ab40f4c25d
thongnguyen <thongnguyen@pixelcent.com>
2023-10-08 16:02:16 +0700

    Upload bai giai mau buoi 7

---
+++ Buoi7/suaBai.py
@@ -0,0 +1,31 @@
+# P2 B2
+# colors = ["Red", "Green", "Blue", "Pink", "Violet"]
+
+# user_input = input("Input: ")
+
+# if user_input.isdigit():
+#     colors.remove(colors[int(user_input)])
+# else:
+#     colors.remove(user_input)
+
+# print(colors)
+
+#p3 b3
+
+# results = 0
+
+# while True:
+#     user_input = int(input("Nhap so: "))
+#     results = results + user_input
+#     if (user_input == 0):
+#         break
+
+# print("Tong: ", results)
+
+#p8 b2
+# numbers = [78, 70, 67, 56, 45, 234, 12312 ,123, 0]
+
+
+# numbers.sort(reverse=True)
+
+# print(numbers[0:5])
